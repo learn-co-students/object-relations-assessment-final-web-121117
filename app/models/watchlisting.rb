dd6582216207bbad7a9efc69615ee39222fcbdd3
github username <github email address>
2018-01-05 15:46:04 -0500

    Done.

--- app/models/watchlisting.rb
+++ app/models/watchlisting.rb
@@ -1,4 +1,29 @@
+# + Watchlisting.all DONE
+#   + returns all of watchlistings
+# + Watchlisting#viewer DONE
+#   + returns the viewer associated with this watchlisting
+# + Watchlisting#movie DONE
+#   + returns the movie associated with this watchlisting
+# + Watchlisting#rating DONE
+#   + returns the viewer's rating for the movie associated with this watchlisting
+
+
+
 class WatchListing
+
+  attr_reader :viewer, :movie, :rating
+  @@all = []
+
+  def initialize(viewer, movie, rating)
+    @viewer = viewer
+    @movie = movie
+    @rating = rating
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 
 end
