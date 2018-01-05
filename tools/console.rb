c919ac372f3d5172f5fe8ce05c995de1d7bc3097
tanelam <tania.e.aparicio@gmail.com>
2018-01-05 15:49:12 -0500

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,17 @@
 require_relative '../config/environment.rb'
+require 'pry'
 
 def reload
   load 'config/environment.rb'
 end
 
-Pry.start
+tania = Viewer.new("tania")
+tan = Viewer.new("tan")
+movie1 = Movie.new("movie1")
+movie2 = Movie.new("movie2")
+list = WatchListing.new(tania, movie1)
+list2= WatchListing.new(tan, movie2)
+
+
+binding.pry
+puts "hello"
